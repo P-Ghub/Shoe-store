d7322c35d125a47a95387e24e48dcbf470211a38
P <philisaninkqeto@gmail.com>
2023-08-29 16:46:39 +0200

    extra '' removed from module.py doc string

--- web_store/store/models.py
+++ web_store/store/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Logos Class
 class storeItem(models.Model):
-    """"This is a class that store info about the 
+    """This is a class that store info about the 
     shoes in the database
 
     :param image: Shoe image
     :param info: Shoe info
     :param brand: Shoe brand
-    """"
+
+    """
 
     image = models.ImageField(upload_to='images/')
     info = models.CharField(max_length=200)
@@ -21,13 +22,13 @@ class storeItem(models.Model):
     
 # Stock Class
 class stockItem(models.Model):
-    """"This is a class that stores info about the 
+    """This is a class that stores info about the 
     stock in the database
 
     :param image: Shoe image
     :param info: Shoe info
     :param brand: Shoe brand
-    """"
+    """
     image = models.ImageField(upload_to='images/')
     info = models.CharField(max_length=200)
     brand = models.CharField(max_length=200, default='brand')
